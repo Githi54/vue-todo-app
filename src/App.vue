587c60d13c30a613c9923e33477df59df0f955c4
Mikita Havryliuk <chinter.games1@gmail.com>
2023-03-02 19:08:28 +0200

    Some fixes

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-import Navbar from './components/Navbar.vue'
+import Navbar from './components/Navbar.vue';
+
 </script>
 
 <template>
-  <Navbar />
+  <Navbar /> 
   <RouterView />
 </template>
 
