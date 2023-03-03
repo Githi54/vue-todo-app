7305bdb685bf1fe967349370ff79546a67e4a5b3
Mikita Havryliuk <chinter.games1@gmail.com>
2023-03-03 10:51:27 +0200

    created delete modal

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,32 +1,15 @@
-<template>
-  <div class="todo" :class="{ completed: todo.completed }">
-    <div class="actions">
-      <h3 @click="showDetails = !showDetails">{{ todo.name }}</h3>
-      <div class="icons">
-        <span class="material-symbols-outlined" @click="showEdit = true">edit</span>
-        <span @click="toggleComplete" class="material-symbols-outlined tick">done</span>
-        <span @click="deletetodo" class="material-symbols-outlined delete">delete</span>
-      </div>
-    </div>
-    <div v-if="showDetails" class="details">
-      <p>{{ todo.description }}</p>
-    </div>
-    <div v-if="showEdit">
-      <EditTodo :id="todo.id" @close="showEdit = false"></EditTodo>
-    </div>
-  </div>
-</template>
-
 <script lang="ts">
 import { useTodoListStore } from '@/stores/todoListStore'
 import EditTodo from './EditTodo.vue'
+import ModalDelete from './ModalDelete.vue'
 
 export default {
   props: ['todo'],
   data() {
     return {
       showDetails: false,
-      showEdit: false
+      showEdit: false,
+      showModal: false
     }
   },
   methods: {
@@ -37,11 +20,35 @@ export default {
     toggleComplete() {
       const store = useTodoListStore()
       store.updateTodoCompleted(this.todo.id)
-    },
+    }
   },
-  components: { EditTodo }
+  components: { EditTodo, ModalDelete }
 }
 </script>
+
+<template>
+  <div class="todo" :class="{ completed: todo.completed }">
+    <div class="actions">
+      <h3 @click="showDetails = !showDetails">{{ todo.name }}</h3>
+      <div class="icons">
+        <span class="material-symbols-outlined" @click="showEdit = true">edit</span>
+        <span @click="toggleComplete" class="material-symbols-outlined tick">done</span>
+        <span @click="showModal = true" class="material-symbols-outlined delete">delete</span>
+      </div>
+    </div>
+    <div v-if="showDetails" class="details">
+      <p>{{ todo.description }}</p>
+    </div>
+    <div v-if="showEdit">
+      <EditTodo :id="todo.id" @close="showEdit = false"></EditTodo>
+    </div>
+    <ModalDelete :show="showModal" @close="showModal = false" :deleteTodo="deletetodo">
+      <template #header>
+        <h3>Are you sure?</h3>
+      </template>
+    </ModalDelete>
+  </div>
+</template>
 
 <style>
 .todo {
